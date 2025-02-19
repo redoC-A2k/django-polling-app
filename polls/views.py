6c313e6e9abdf816717c6cc5195e7c7181368c8c
afshan ahmed khan <afshanahmeda2k@gmail.com>
2025-02-19 16:49:37 +0530

    Part 5 added test functionality

--- polls/views.py
+++ polls/views.py
@@ -5,13 +5,15 @@ from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
 from django.template import loader
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        # return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 class DetailView(generic.DetailView):
     model = Question
